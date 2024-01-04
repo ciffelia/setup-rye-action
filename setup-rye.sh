05fc34bd1233263aa67820949fb7171c6e41e9b6
Ciffelia <mc.prince.0203@gmail.com>
2024-01-04 18:48:20 +0900

    Initial commit

---
+++ setup-rye.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
+
+TEMP_FILE=$(mktemp)
+
+curl -fsSL "https://github.com/mitsuhiko/rye/releases/download/$RYE_VERSION/rye-x86_64-linux.gz" | gunzip > "$TEMP_FILE"
+chmod +x "$TEMP_FILE"
+"$TEMP_FILE" self install --yes
+
+rm "$TEMP_FILE"
+
+echo "$HOME/.rye/shims" >> "$GITHUB_PATH"
